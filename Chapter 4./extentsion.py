47714f177696bcf1c730bf7c536d080a9303fd83
Lichyo <chyoxuan@gmail.com>
2022-11-26 13:01:01 +0800

    Chapter 4 homework.

---
+++ Chapter 4./extentsion.py
@@ -0,0 +1,16 @@
+sum = 0
+sum += 0
+sum += 1
+sum += 2
+sum += 3
+print(sum)
+
+
+# \ 用來延續
+sum = 0
+sum = 1 + \
+    2 + \
+    3 + \
+sum
+
+print(sum)
---
+++ Chapter 4./homework.py
@@ -0,0 +1,9 @@
+secret = 5
+guess = 3
+
+if guess > secret:
+    print('too high')
+elif guess == secret:
+    print('just right')
+else:
+    print('too low')
